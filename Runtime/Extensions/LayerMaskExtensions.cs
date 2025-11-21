805ecde918a886b482b6a0409660bd639aca85e6
Daniel Loughmiller <itsdan@gmail.com>
2025-11-21 14:09:29 -0500

    Add more layermask extensions

--- Runtime/Extensions/LayerMaskExtensions.cs
+++ Runtime/Extensions/LayerMaskExtensions.cs
@@ -7,5 +7,34 @@ namespace DGP.UnityExtensions
         public static bool Contains(this LayerMask layerMask, int layer) {
             return layerMask == (layerMask | (1 << layer));
         }
+        
+        public static LayerMask AddLayer(this LayerMask layerMask, int layer) {
+            layerMask |= (1 << layer);
+            return layerMask;
+        }
+        
+        public static LayerMask RemoveLayer(this LayerMask layerMask, int layer) {
+            layerMask &= ~(1 << layer);
+            return layerMask;
+        }
+        
+        public static LayerMask ToggleLayer(this LayerMask layerMask, int layer) {
+            if (layerMask.Contains(layer)) {
+                layerMask = layerMask.RemoveLayer(layer);
+            } else {
+                layerMask = layerMask.AddLayer(layer);
+            }
+            return layerMask;
+        }
+        
+        public static int[] ToLayerArray(this LayerMask layerMask) {
+            var layers = new System.Collections.Generic.List<int>();
+            for (int i = 0; i < 32; i++) {
+                if (layerMask.Contains(i)) {
+                    layers.Add(i);
+                }
+            }
+            return layers.ToArray();
+        }
     }
 }
