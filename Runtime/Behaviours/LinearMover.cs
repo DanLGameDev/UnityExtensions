b28eafb9c0b17724a1fd5674cd3980110de0c85c
Daniel Loughmiller <itsdan@gmail.com>
2026-02-02 09:44:54 -0500

    implement destroy after delay

--- Runtime/Behaviours/LinearMover.cs
+++ Runtime/Behaviours/LinearMover.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
 
 namespace DGP.UnityExtensions.Behaviours
 {
@@ -10,11 +13,27 @@ namespace DGP.UnityExtensions.Behaviours
         
         [SerializeField] private Vector3 direction = Vector3.right;
         [SerializeField] private float speed = 1f;
-        [SerializeField] private float destroyDelaySeconds = 5f;
+        
+        [SerializeField] private bool destroyAfterDelay = false;
+        [SerializeField]
+        #if ODIN_INSPECTOR
+        [ShowIf(nameof(destroyAfterDelay))]
+        #endif
+        private float destroyDelaySeconds = 5f;
+        
+        private float _elapsedTime = 0f;
         
         private void Update()
         {
             transform.position += direction.normalized * (speed * Time.deltaTime);
+
+            if (!destroyAfterDelay) 
+                return;
+            
+            _elapsedTime += Time.deltaTime;
+                
+            if (_elapsedTime >= destroyDelaySeconds)
+                Destroy(gameObject);
         }
     }
 }
